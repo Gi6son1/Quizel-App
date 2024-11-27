12d06a91b7e5c3ebaefca2d1f0fe1136228b5128
Owain Gibson <owain.gibson@hotmail.co.uk>
2024-11-27 22:44:10 +0000

    added back surfaces

--- app/src/main/java/com/cs31620/quizel/ui/questionbank/QuestionBankScreen.kt
+++ app/src/main/java/com/cs31620/quizel/ui/questionbank/QuestionBankScreen.kt
@@ -1,12 +1,21 @@
 package com.cs31620.quizel.ui.questionbank
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,11 +31,34 @@ fun QuestionBankScreen(navController: NavHostController) {
     ) { innerPadding ->
         Surface(
             modifier = Modifier
-                .fillMaxSize()
+                .padding(10.dp)
                 .padding(innerPadding),
             color = Color.Transparent
         ) {
-            Text(text = "Question Bank Screen", modifier = Modifier.padding(start = 8.dp))
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Button(onClick = {}, Modifier.weight(1.1f)
+                    .fillMaxWidth())
+                {
+                    Text(text = "New Question")
+                }
+                Surface(
+                    Modifier.weight(10f)
+                        .padding(top = 8.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(5)),
+                    color = MaterialTheme.colorScheme.surfaceContainer,
+
+                    ) {
+                    Text(text = "Question Bank Screen", modifier = Modifier.padding(start = 8.dp))
+                }
+
+            }
+
         }
     }
 }
