a2ca8bb26ffbc2b3cd0bf85bf6fed2dce26d6c6a
Owain Gibson <owain.gibson@hotmail.co.uk>
2024-12-20 08:24:51 +0000

    added a few buttons

--- app/src/main/java/com/cs31620/quizel/ui/takequiz/TestQuestionsScreen.kt
+++ app/src/main/java/com/cs31620/quizel/ui/takequiz/TestQuestionsScreen.kt
@@ -35,9 +35,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavHostController
 import com.cs31620.quizel.model.QuestionsViewModel
 import com.cs31620.quizel.ui.components.ActionCheckDialog
@@ -62,6 +65,14 @@ fun TestQuestionsScreenTopLevel(
             navController.navigate(destination){
                 launchSingleTop = true
             }
+        },
+
+        exitQuiz = { exit ->
+            if (exit) {
+                navController.navigate(Screen.TakeQuiz.route) {
+                    launchSingleTop = true
+                }
+            }
         }
     )
 }
@@ -69,7 +80,8 @@ fun TestQuestionsScreenTopLevel(
 @Composable
 private fun TestQuestionsScreen(
     questionList: List<Question>,
-    quizResultsScreen: (String) -> Unit = {}
+    quizResultsScreen: (String) -> Unit = {},
+    exitQuiz: (Boolean) -> Unit = {},
 ) {
     TopLevelBackgroundScaffold { innerPadding ->
 
@@ -80,6 +92,7 @@ private fun TestQuestionsScreen(
 
         var selectedAnswer by rememberSaveable (currentQuestion) { mutableStateOf<Answer?>(null) }
         var showSkipDialog by rememberSaveable { mutableStateOf(false) }
+        var showExitQuizDialog by rememberSaveable { mutableStateOf(false) }
 
         val shuffledAnswers = rememberSaveable (currentQuestion) {
             currentQuestion.answers.shuffled()
@@ -99,147 +112,186 @@ private fun TestQuestionsScreen(
             }
         }
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize()
                 .padding(innerPadding)
-                .padding(start = 10.dp, end = 10.dp, top = 100.dp, bottom = 20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(20.dp)
+                .padding(start = 10.dp, end = 10.dp)
         ) {
-            LinearProgressIndicator(
-                modifier = Modifier
-                    .weight(0.25f)
-                    .fillMaxWidth(),
-                progress = {currentQuestionNumber/totalQuestions.toFloat()}
-            )
-            Surface(
-                modifier = Modifier
-                    .weight(11f)
-                    .shadow(10.dp, MaterialTheme.shapes.medium),
-                shape = MaterialTheme.shapes.medium,
-            )
-            {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    Text(
-                        modifier = Modifier.padding(bottom = 10.dp),
-                        text = if (currentQuestion.title.isBlank())
-                        {
-                            "Question $currentQuestionNumber"
+            val (questionAnswerSubmitColumn, homeButton, currentScoreText) = createRefs()
 
-                        } else {
-                            currentQuestion.title
-                        }
-                    )
-                    HorizontalDivider()
-                    Text(
-                        text = currentQuestion.description
-                    )
-                }
+            Button(
+                onClick =
+                    {
+                        showExitQuizDialog = true
+                    },
+                modifier = Modifier
+                    .constrainAs(homeButton){
+                        top.linkTo(parent.top, margin = 20.dp)
+                        start.linkTo(parent.start, margin = 20.dp)
+                    }
+            ) {
+                Text("Exit")
             }
-            Surface(modifier = Modifier
-                .weight(10f)
-                .shadow(10.dp, MaterialTheme.shapes.medium),
-                color = Color.LightGray,
-                shape = MaterialTheme.shapes.medium,
-                )
-            {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
+
+            Text(
+                text = "Score: $currentScore/$totalQuestions",
+                modifier = Modifier
+                    .constrainAs(currentScoreText) {
+                        top.linkTo(parent.top, margin = 20.dp)
+                        end.linkTo(parent.end, margin = 20.dp)
+                    }
+            )
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 40.dp)
+                    .constrainAs(questionAnswerSubmitColumn) {
+                        top.linkTo(homeButton.bottom, margin = 80.dp)
+                        top.linkTo(currentScoreText.bottom, margin = 80.dp)
+                        bottom.linkTo(parent.bottom)
+                        height = Dimension.fillToConstraints
+                    },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                LinearProgressIndicator(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(start = 5.dp, end = 5.dp, top = 5.dp),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalArrangement = Arrangement.spacedBy(5.dp)
-                ) {
-                    items(shuffledAnswers) { answer ->
-                        Button(
-                            onClick = {
-                                selectedAnswer = if (selectedAnswer != answer) {
-                                    answer
-                                } else {
-                                    null
-                                }
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .weight(1f)
-                                .height(80.dp)
-                                .shadow(10.dp, MaterialTheme.shapes.medium),
-                            shape = MaterialTheme.shapes.medium,
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = if (selectedAnswer == answer) {
-                                    MaterialTheme.colorScheme.primary
-                                } else {
-                                    MaterialTheme.colorScheme.surfaceContainer
-                                }
-                            )
-                        ) {
-                            Text(text = answer.text,
-                                color = if (selectedAnswer == answer){
-                                    Color.White
-                                } else {
-                                    Color.Black
+                        .weight(0.25f)
+                        .fillMaxWidth(),
+                    progress = {currentQuestionNumber/totalQuestions.toFloat()}
+                )
+                Surface(
+                    modifier = Modifier
+                        .weight(11f)
+                        .shadow(10.dp, MaterialTheme.shapes.medium),
+                    shape = MaterialTheme.shapes.medium,
+                )
+                {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(bottom = 10.dp),
+                            text = if (currentQuestion.title.isBlank())
+                            {
+                                "Question $currentQuestionNumber"
+
+                            } else {
+                                currentQuestion.title
+                            }
+                        )
+                        HorizontalDivider()
+                        Text(
+                            text = currentQuestion.description
+                        )
+                    }
+                }
+                Surface(modifier = Modifier
+                    .weight(10f)
+                    .shadow(10.dp, MaterialTheme.shapes.medium),
+                    color = Color.LightGray,
+                    shape = MaterialTheme.shapes.medium,
+                )
+                {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 5.dp, end = 5.dp, top = 5.dp),
+                        horizontalArrangement = Arrangement.spacedBy(5.dp),
+                        verticalArrangement = Arrangement.spacedBy(5.dp)
+                    ) {
+                        items(shuffledAnswers) { answer ->
+                            Button(
+                                onClick = {
+                                    selectedAnswer = if (selectedAnswer != answer) {
+                                        answer
+                                    } else {
+                                        null
+                                    }
                                 },
-                                fontSize = 15.sp
-                            )
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                                    .height(80.dp)
+                                    .shadow(10.dp, MaterialTheme.shapes.medium),
+                                shape = MaterialTheme.shapes.medium,
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = if (selectedAnswer == answer) {
+                                        MaterialTheme.colorScheme.primary
+                                    } else {
+                                        MaterialTheme.colorScheme.surfaceContainer
+                                    }
+                                )
+                            ) {
+                                Text(text = answer.text,
+                                    color = if (selectedAnswer == answer){
+                                        Color.White
+                                    } else {
+                                        Color.Black
+                                    },
+                                    fontSize = 15.sp
+                                )
+                            }
                         }
                     }
                 }
-            }
-            if (selectedAnswer != null) {
-                Button(
-                    onClick = {
-                        if (selectedAnswer!!.isCorrect){
-                            nextQuestion(true)
-                        } else {
-                            nextQuestion()
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(2f)
-                        .shadow(10.dp, MaterialTheme.shapes.medium),
-                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-                    shape = MaterialTheme.shapes.medium
-                ) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.Send,
-                        contentDescription = "Submit icon",
+                if (selectedAnswer != null) {
+                    Button(
+                        onClick = {
+                            if (selectedAnswer!!.isCorrect){
+                                nextQuestion(true)
+                            } else {
+                                nextQuestion()
+                            }
+                        },
                         modifier = Modifier
-                            .wrapContentSize(),
-                    )
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text(text = "Submit Answer")
-                }
-            } else {
-                Button(
-                    onClick = { showSkipDialog = true },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(2f)
-                        .shadow(10.dp, MaterialTheme.shapes.medium)
-                        .border(4.dp, Color.LightGray),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.LightGray),
-                    shape = MaterialTheme.shapes.medium
-                ) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.Send,
-                        contentDescription = "Submit icon",
+                            .fillMaxWidth()
+                            .weight(2f)
+                            .shadow(10.dp, MaterialTheme.shapes.medium),
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+                        shape = MaterialTheme.shapes.medium
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.Send,
+                            contentDescription = "Submit icon",
+                            modifier = Modifier
+                                .wrapContentSize(),
+                        )
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(text = "Submit Answer")
+                    }
+                } else {
+                    Button(
+                        onClick = { showSkipDialog = true },
                         modifier = Modifier
-                            .wrapContentSize(),
-                        tint = Color.Black
-                    )
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text(text = "Skip Question",
-                        color = Color.Black)
+                            .fillMaxWidth()
+                            .weight(2f)
+                            .shadow(10.dp, MaterialTheme.shapes.medium)
+                            .border(4.dp, Color.LightGray),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.LightGray),
+                        shape = MaterialTheme.shapes.medium
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.Send,
+                            contentDescription = "Submit icon",
+                            modifier = Modifier
+                                .wrapContentSize(),
+                            tint = Color.Black
+                        )
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(text = "Skip Question",
+                            color = Color.Black)
+                    }
                 }
             }
         }
+
+
         ActionCheckDialog(
             dialogIsOpen = showSkipDialog,
             dialogOpen = { isOpen -> showSkipDialog = isOpen },
@@ -247,7 +299,7 @@ private fun TestQuestionsScreen(
                 Button(
                     onClick = onClick,
                     modifier = modifier,
-                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiary)
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
                 ) {
                     Text(text = "Skip", fontSize = 19.sp)
                 }
@@ -257,6 +309,24 @@ private fun TestQuestionsScreen(
                 if (skipQuestion) {
                     nextQuestion()
                 }
+            }
+        )
+
+        ActionCheckDialog(
+            dialogIsOpen = showExitQuizDialog,
+            dialogOpen = { isOpen -> showExitQuizDialog = isOpen },
+            mainActionButton = { onClick, modifier ->
+                Button(
+                    onClick = onClick,
+                    modifier = modifier,
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+                ) {
+                    Text(text = "Exit Quiz", fontSize = 19.sp)
+                }
+            },
+            actionDialogMessage = "Are you sure you want to exit? Your quiz progress will not be saved",
+            performMainAction = { exit ->
+                exitQuiz(exit)
             }
         )
     }
